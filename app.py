eed1608ed9831c95bd8d4ac646c5c0ab81cfde94
kathorak <146837122+kathorak@users.noreply.github.com>
2024-12-04 09:32:44 +0100

    Update app.py
    přepis kódu

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request #importování potřebných knihoven
 
-app = Flask(__name__)
-
-
-@app.route("/")
+app = Flask(__name__) #zajistí spuštění aplikace
 
 
+@app.route("/") #UMĚT "/" !!! dekorátor (zlepšuje/upravuje funkci)
+def home():
+    return render_template("index.html")
 
-# name = request.args.get("name") 
-# input_class = request.args.get("class")
-# message = request.args.get("message")
+@app.route("/form")
+def form():
+    py_var = request.args.get("html_name")
+    return render_template("form.html", jinja_var=py_var)
+    
+# name = request.args.get("name")   #Proměnná "name" který přijme jméno uživatele napsané do formuláře
+# input_class = request.args.get("class") #Proměnná "input_class" který přijímá třídu uživatele napsanou do formuláře
+# message = request.args.get("message") #Proměnná "message" přijme zprávu od uživatele napsanou do formuláře
 
-#     return redirect(url_for("result", name=name, form_class=input_class, message=message))
+#     return redirect(url_for("result", name=name, form_class=input_class, message=message)) #
     
 
 
@@ -19,12 +24,12 @@ app = Flask(__name__)
 # input_class = request.form.get("class")
 # message = request.form.get("message")
 
-#     return redirect(url_for("result", name=name, form_class=input_class, message=message))
+#     return redirect(url_for("result", name=name)
     
 
 
 
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": #zkontroluje zda je to spuštěné přímo nebo klonované
     app.run(debug=True) #spouštění flaskové aplikace
